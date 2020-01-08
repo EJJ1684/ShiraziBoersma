582e044ab129457603063ab802e98a6abd275236
EJJ1684 <42565657+EJJ1684@users.noreply.github.com>
2020-01-08 16:14:48 +1100

    Add files via upload

---
+++ R/sbTT_iuss.R
@@ -0,0 +1,27 @@
+sbTT_iuss <-
+function(add=F,cols="black",size=1){
+  lines_out = iuss_lines
+  x = iuss_x
+  y = iuss_y
+  
+  if(!add){plot(NA,cex=0.0, xlim=c(-4,0), ylim=c(1,10),ylab="Geometric standard deviation", xlab="Geometric mean diameter (mm)", xaxt="n",xaxs="i")
+    axis(1,at=c(-4,-3,-2,-1,0), labels=c("0.0001","0.001","0.01","0.1","1.0"),lwd.ticks=2)
+    axis(1,at=log10(1:10)-1, labels=F)
+    axis(1,at=log10(1:10)-2, labels=F)
+    axis(1,at=log10(1:10)-3, labels=F)
+    axis(1,at=log10(1:10)-4, labels=F)
+  }
+
+
+  for(i in 1:length(lines_out)){
+    lines(lines_out[[i]][,1],lines_out[[i]][,2], col=cols, lwd=size)
+  }
+  
+  ###Ids at polygon centroid
+  vert_tab=TT.classes.tbl(class.sys = "ISSS.TT", collapse = NULL)
+  for(r in 1:nrow(vert_tab)){
+    ids=as.numeric(unlist(str_split(vert_tab[r,3],", ")))
+    text(TT.polygon.centroids(x[ids], y[ids])[1],
+         TT.polygon.centroids(x[ids], y[ids])[2],vert_tab[r,1], col=cols, cex=size)
+  }  
+}
---
+++ R/shb_calc.R
@@ -0,0 +1,74 @@
+shb_calc <-
+function(psa,lim,plt,seg_dat){
+
+  ##limit (mm) of clay, silt and sand fraction from USDA texture limits
+  s0=0.00005 ##clay lower size limit sometimes given as 0.00001, other times 0.00005. can dramatically chage results
+  sizes=c(s0,lim)
+
+  ###probabilties
+  p_3 = 0.9999 ##probability of upper size limit
+  p_0 = 0.0001 ##probability of lower size limit
+  ##cummulative clay and silt
+  probs=c(p_0,cumsum(psa[-length(psa)])/100,p_3)
+  probs[which(probs<p_0)]=p_0 ##min probability= 0.0001
+  probs[which(probs>p_3)]=p_3 ##max probability= 0.9999
+
+
+  ###functions
+  d_lognorm  = function(x) {exp(-(log10(x)-a)^2/(2*b^2))/(b*sqrt(2*pi))}
+  integrand0 = function(x) {exp(-(x-a)^2/(2*b^2))/(b*sqrt(2*pi))}
+  integrand1 = function(x) {exp(-(x-a)^2/(2*b^2))/(b*sqrt(2*pi))*x}
+  integrand2 = function(x) {exp(-(x-a)^2/(2*b^2))/(b*sqrt(2*pi))*x^2}
+
+  ##integrated moments
+  m0=c()
+  m1=c()
+  m2=c()
+
+  ##run it
+  if(plt){
+    plot(NA,xlim=c(log10(sizes[1]),log10(10)),ylim=c(0,12),yaxs="i",xaxs="i", ylab="Theoretical lognormal prob.", xlab="Grain size (mm)", xaxt="n")
+    axis(1,at= -5:1, labels=c("0.00001","0.0001","0.001","0.01","0.1","1.0","10.0"))
+    abline(v=log10(sizes[2:(length(sizes))]),lty=2, col="grey")
+  }
+
+  if(seg_dat){cat("\nSegment d_g\tSeg. sigma_g")} ##d_g and sigma of segment
+  for(i in 1:length(psa)){ #i=1
+    ##Normal deviates
+    x1 = log10(sizes[i])
+    Y1 = qnorm(probs[i])
+    x2 = log10(sizes[i+1])
+    Y2 = qnorm(probs[i+1])
+
+    ##calculate values
+    b=(x2-x1)/(Y2-Y1)
+    a=-Y1*b+x1
+
+    if(!is.nan(a)&!is.infinite(a)){
+
+      if(seg_dat){cat("\n",10^a,"\t",10^b,"\n")} ##d_g and sigma of segment
+
+      ##plot fitted distributions for each segment
+      if(plt){
+        vals=seq(log10(sizes[i]),log10(sizes[i+1]),length.out =1000)
+        nums=seq(log10(sizes[1]),log10(100),length.out =1000)
+        lines(nums,d_lognorm(10^nums)*10, lty=2) ##dodgy *10 constant, I cannot produce theier results without it
+        lines(vals,d_lognorm(10^vals)*10, lwd=2) ##dodgy *10 constant, I cannot produce theier results without it
+      }
+
+      m0=c(m0,integrate(integrand0, lower = log10(sizes[i]), upper = log10(sizes[i+1]))$value)
+      m1=c(m1,integrate(integrand1, lower = log10(sizes[i]), upper = log10(sizes[i+1]))$value)
+      m2=c(m2,integrate(integrand2, lower = log10(sizes[i]), upper = log10(sizes[i+1]))$value)
+    }else{
+      if(seg_dat){cat("\n",0,"\t",0)} ##d_g and sigma of segment
+    }
+  }
+  area=sum(m0)
+  a=sum(m1)/area
+  b=sqrt(sum(m2)/area-a^2)
+
+  d_g=10^a
+  sigma_g=10^b
+
+  return(data.frame("d_g"=d_g,"sigma_g"=sigma_g))
+}
