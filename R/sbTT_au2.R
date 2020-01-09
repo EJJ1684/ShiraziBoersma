03430c897c5931b674467b126ff62481058cdfff
EJJ1684 <42565657+EJJ1684@users.noreply.github.com>
2020-01-09 11:36:01 +1100

    Update sbTT_au2.R

--- R/sbTT_au2.R
+++ R/sbTT_au2.R
@@ -1,6 +1,8 @@
 sbTT_au2 <-
 function(add=F,cols="black",size=1){
-  data("au2")
+  linesout = ShiraziBoersma:::au2_lines_out
+  x = ShiraziBoersma:::au2_x
+  y = ShiraziBoersma:::au2_y
   
   if(!add){plot(NA,cex=0.0, xlim=c(-4,0), ylim=c(1,10),ylab="Geometric standard deviation", xlab="Geometric mean diameter (mm)", xaxt="n",xaxs="i")
     axis(1,at=c(-4,-3,-2,-1,0), labels=c("0.0001","0.001","0.01","0.1","1.0"),lwd.ticks=2)
